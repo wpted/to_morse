76079ae7529f48e0979e4a255369753bf503a92a
wpted <silverhandsome@gmail.com>
2022-04-28 23:17:50 +0800

    Add testing in main.py, repeat the program if an exception occur.

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from text import Text
 
 def main():
     print("Welcome to the Morse Code Converter!")
+    print("***")
 
-    text_to_transfer = str(input("What text do you want to convert?"))
+    text_to_transfer = str(input("What text do you want to convert: "))
     text = Text(text_to_transfer)
-    print(text.text_to_morse())
+
+    try:
+        print("***")
+        print(f"Morse code of '{text.text}' is '{text.to_morse()}'.")
+        print("***")
+
+    except Exception as e:
+        print(f"Message: {e.__class__.__name__} {e}")
+        print("Try again!")
+        main()
 
 
 if __name__ == "__main__":
